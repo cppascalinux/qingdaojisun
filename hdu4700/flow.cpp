377033257c7d5041e6ac071d5d55d4d8c09d81c6
cppascalinux <cppascalinux@gmail.com>
2019-03-02 22:28:05 +0800

    qwq

--- hdu4700/flow.cpp
+++ hdu4700/flow.cpp
@@ -10,35 +10,38 @@ int f[109][109],g[109][109];
 int dis[109],vis[109],pre[109];
 void prim()
 {
-	memset(dis,0x7F,sizeof(dis));
+	memset(dis,0x80,sizeof(dis));
 	memset(vis,0,sizeof(vis));
 	memset(pre,0,sizeof(pre));
-	memset(f,0,sizeof(f));
+	memset(f,-1,sizeof(f));
 	dis[1]=0;
 	for(int i=1;i<=n;i++)
 	{
 		int p=0;
 		for(int j=1;j<=n;j++)
-			if(!vis[j]&&dis[j]<dis[p])
+			if(!vis[j]&&dis[j]>dis[p])
 				p=j;
 		vis[p]=1;
 		f[pre[p]][p]=f[p][pre[p]]=mat[p][pre[p]];
 		for(int j=1;j<=n;j++)
-			if(!vis[j]&&dis[j]>mat[p][j])
+			if(!vis[j]&&dis[j]<mat[p][j])
 			{
 				dis[j]=mat[p][j];
 				pre[j]=p;
 			}
 	}
+	// for(int i=1;i<=n;i++)
+	// {
+	// 	for(int j=1;j<=n;j++)
+	// 		printf("%d ",f[i][j]);
+	// 	printf("\n");
+	// }
 }
 void dfs(int x,int fa,int st,int mn)
 {
-	if(x==st)
-		g[st][x]=-1;
-	else
-		g[st][x]=mn;
+	g[st][x]=x==st?-1:mn;
 	for(int i=1;i<=n;i++)
-		if(i!=fa&&f[x][i]>0)
+		if(i!=fa&&f[x][i]>-1)
 			dfs(i,x,st,min(mn,f[x][i]));
 }
 int main()
@@ -68,7 +71,7 @@ int main()
 			for(int i=1;i<=n;i++)
 			{
 				for(int j=1;j<=n;j++)
-					printf("%d ",i==j?-1:f[i][j]);
+					printf("%d ",i==j?-1:(f[i][j]==-1?0:f[i][j]));
 				printf("\n");
 			}
 		}
